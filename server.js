f308ca1eca6abc9603f30ec93ba36f921e90af41
beakerandjake <jhoff462@gmail.com>
2017-10-28 15:30:43 -0700

    port supplied by env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', function(request, response){
     response.send('Hello world!');
 });
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log('Listening on port 3000.');
 });
