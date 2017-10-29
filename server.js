fa33379a93dabf894760d18c10865a624874de86
beakerandjake <jhoff462@gmail.com>
2017-10-28 17:31:36 -0700

    add cors middleware

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const questionTrackRoutes = require('./api/routes/question-track.routes');
+
+// Configure the application to use cors. 
+app.use(cors());
 
 // Add the routing for the question track. 
 questionTrackRoutes(app);
