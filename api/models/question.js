b620cccf238ad4ed4c13ce5402191b568728bca6
beakerandjake <jhoff462@gmail.com>
2017-11-04 11:19:07 -0700

    add status

--- api/models/question.js
+++ api/models/question.js
@@ -8,7 +8,13 @@ const QuestionSchema = Schema({
         get: v => Math.round(v),
         set: v => Math.round(v),
         min: 1
-    }
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Locked', 'Unlocked', 'Current'],
+        default: 'Locked'
+    },
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
