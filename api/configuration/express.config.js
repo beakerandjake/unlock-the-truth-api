ac9eaabeb0a233c156d15f5660da303085d88227
beakerandjake <jhoff462@gmail.com>
2017-11-18 11:28:55 -0700

    configure session / cors

--- api/configuration/express.config.js
+++ api/configuration/express.config.js
@@ -1,13 +1,36 @@
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+const dbConfig = require('../configuration/db.config');
 
 // Configure our express application
 
 module.exports = function (app, passport) {
+    // Add basic extensions to our app  
     app.use(helmet());
-    app.use(cors());
+    app.use(cors({
+        origin: ['http://localhost:8080'],
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+        credentials: true
+    }));
     app.use(bodyParser.json());
 
-    // Cookie/Session
-}
+    // configure express to use session and save in our db. 
+    app.use(session({
+        name: 'utt.session',
+        resave: false,
+        saveUninitialized: true,
+        secret: 'keyboard cat',
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection,
+            collection: 'sessions'
+        })
+    }));
+
+    // configure express to use passport session
+    app.use(passport.initialize());
+    app.use(passport.session());
+};
