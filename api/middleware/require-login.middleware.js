56fbd8a481eb06564783f3b9f4f6e47e80c1b947
beakerandjake <jhoff462@gmail.com>
2017-11-18 14:03:08 -0700

    invoke error

--- api/middleware/require-login.middleware.js
+++ api/middleware/require-login.middleware.js
@@ -1,10 +1,13 @@
 // Middleware which requires rejects any request which doesn't contain a user. 
 module.exports = function (request, response, next) {
-    if(request.user){
+    if (request.user) {
         // Allow the request to continue if we have a user object.
         next();
     } else {
         // Return a 401 if we don't have a user. 
-        response.sendStatus(401);
+        next({
+            status: 401,
+            message: 'You must be logged in to perform this action!'
+        });
     }
 }
