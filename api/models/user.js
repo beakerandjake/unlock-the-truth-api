231ce6736f51a0ccef88d66813740138919d67fe
beakerandjake <jhoff462@gmail.com>
2017-11-18 09:43:05 -0700

    add validate method

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Represents a user, allows users to log into the website.  
+
 const UserSchema = Schema({
     username: {
         type: String,
@@ -11,5 +13,11 @@ const UserSchema = Schema({
         required: true
     }
 });
+
+// Does the provided password match the stored password?  
+UserSchema.methods.validPassword = function (plainTextPassword) {
+    // TODO hash  
+    return plainTextPassword === this.password;
+};
 
 module.exports = mongoose.model('User', UserSchema);
