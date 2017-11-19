721ab0013b00a91b1bb5765b9f2b766b06d1b1be
beakerandjake <jhoff462@gmail.com>
2017-11-18 17:16:14 -0700

    no need for cors config right now

--- api/configuration/express.config.js
+++ api/configuration/express.config.js
@@ -6,13 +6,8 @@ const passport = require('passport');
 // Configure our express application
 
 module.exports = function (app) {
-    // Add basic extensions to our app  
     app.use(helmet());
-    app.use(cors({
-        origin: ['http://localhost:8080'],
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        credentials: true
-    }));
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(passport.initialize());
 };
