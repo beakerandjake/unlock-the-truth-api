3786bd3675c0bbd1d563503da05fc9cfdc46e9e2
beakerandjake <jhoff462@gmail.com>
2017-11-19 19:19:07 -0700

    add rate limiter

--- api/configuration/routes.config.js
+++ api/configuration/routes.config.js
@@ -1,9 +1,11 @@
+const errorHandler = require('api-error-handler');
+const expressLimiter = require('express-limiter');
+const redisClient = require('redis').createClient();
 const questionTrackRoutes = require('../routes/question-track.routes');
 const theTruthRoutes = require('../routes/the-truth.routes');
 const userRoutes = require('../routes/user.routes');
 const authErrorHandler = require('../middleware/auth-error.middleware');
 const notFoundErrorHandler = require('../middleware/not-found.middleware');
-const errorHandler = require('api-error-handler');
 
 // Configure our APIs routing. 
 
@@ -12,6 +14,9 @@ module.exports = function (app) {
     app.use('/api/questions', questionTrackRoutes);
     app.use('/api/thetruth', theTruthRoutes);
     app.use('/api/user', userRoutes);
+
+    // Put login route behind rate limiter.
+    addRateLimiter(app, '/api/user');
 
     // Auth error handling
     app.use(authErrorHandler);
@@ -22,3 +27,21 @@ module.exports = function (app) {
     // Add error handler which returns JSON.
     app.use(errorHandler());
 };
+
+// Add rate limiter to specified route.  
+function addRateLimiter(app, route) {
+    // Create our rate limiter
+    const rateLimiter = expressLimiter(app, redisClient);
+
+    var limiter = require('express-limiter')(app, client);
+
+    // Protect the login route behind rate limiter.
+    limiter({
+        path: route,
+        method: 'get',
+        lookup: ['connection.remoteAddress'],
+        // 150 requests per hour
+        total: 150,
+        expire: 1000 * 60 * 60
+    });
+}
